9d85ff40b6c3bd45450c33e7847627cb0bf46ca3
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-07-10 18:35:33 +0530

    Input Animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -23,6 +24,17 @@ const CreatePostWizard = () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
     },
+
+    onError: (err) => {
+      const error = err.data?.zodError?.fieldErrors.content;
+
+      if (error && error[0]) {
+        toast.error(error[0]);
+        return;
+      }
+
+      toast.error("Something went wrong. Please try again later.");
+    },
   });
 
   return (
@@ -38,15 +50,30 @@ const CreatePostWizard = () => {
         placeholder="Enter an emoji..."
         className="grow bg-transparent outline-none"
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") mutate({ content: input });
+          }
+        }}
         disabled={isPosting}
         value={input}
       />
-      <button
-        onClick={() => mutate({ content: input })}
-        className="rounded-md bg-blue-500 px-4 py-2 text-white"
-      >
-        Post
-      </button>
+      {input !== "" && !isPosting && (
+        <button
+          onClick={() => mutate({ content: input })}
+          className="rounded-md bg-blue-500 px-4 py-2 text-white"
+          disabled={isPosting}
+        >
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={25} />
+        </div>
+      )}
     </div>
   );
 };
